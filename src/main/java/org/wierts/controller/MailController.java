eee1c58ab4fc8b0b9350bb67f32acda70485658a
DESKTOP-CCDS2EU\natas <natasha_sheko@yahoo.com>
2024-06-14 22:12:42 +0200

    save the file

--- src/main/java/org/wierts/controller/MailController.java
+++ src/main/java/org/wierts/controller/MailController.java
@@ -1,17 +1,19 @@
 package org.wierts.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.wierts.dto.Message;
 import org.wierts.service.MessageService;
 
 @RestController
 public class MailController {
-
+    @Autowired
     private MessageService messageService;
     @PostMapping ("/send")
-    public Message sendEmail(@RequestParam(value = "message") Message message) {
+    public Message sendEmail(@RequestBody Message message) throws Exception{
         messageService.sendEmail(message);
         return message;
     }
--- src/main/java/org/wierts/service/MessageService.java
+++ src/main/java/org/wierts/service/MessageService.java
@@ -1,44 +1,23 @@
 package org.wierts.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Service;
 import org.wierts.dto.Message;
 
-import java.util.Properties;
+import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 
 @Service
 public class MessageService {
 
-    @Autowired
-    private JavaMailSender mailSender;
-    public void sendEmail(Message message){
-        SimpleMailMessage mainMessage = new SimpleMailMessage();
-        mainMessage.setTo("nataliya.wierts@gmail.com");
-        mainMessage.setSubject("homepage kontakt");
-        mainMessage.setText(message.getEmail()+" "+message.getMessage());
-
-        mailSender.send(mainMessage);
+    public void sendEmail(Message message) throws Exception {
+        LocalDateTime time = LocalDateTime.now();
+        File file = new File(time.format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HHmmssSSSSSS"))+".txt");
+        FileUtils.writeStringToFile(file, message.getEmail()+" "+message.getMessage());
     }
 
-    @Bean
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
 
-        mailSender.setUsername("nataliya.wierts@gmail.com");
-        mailSender.setPassword("");
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
-
-        return mailSender;
-    }
 }
