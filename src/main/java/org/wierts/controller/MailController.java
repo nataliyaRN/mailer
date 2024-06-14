ebc9c7fdb20011b6fbfb836f3fd1836caa4baa4c
DESKTOP-CCDS2EU\natas <natasha_sheko@yahoo.com>
2024-06-15 00:27:17 +0200

    cross origin

--- src/main/java/org/wierts/controller/MailController.java
+++ src/main/java/org/wierts/controller/MailController.java
@@ -1,6 +1,7 @@
 package org.wierts.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -13,6 +14,7 @@ public class MailController {
     @Autowired
     private MessageService messageService;
     @PostMapping ("/send")
+    @CrossOrigin(origins = {"http://localhost:3000", "http://localhost:80"})
     public Message sendEmail(@RequestBody Message message) throws Exception{
         messageService.sendEmail(message);
         return message;
